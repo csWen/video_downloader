52d6cf882db2f80513d453aa0e5592b38e836476
csWen <707977597@qq.com>
2019-06-30 23:22:55 +0800

    Only youtube.

--- routes/index.js
+++ routes/index.js
@@ -16,53 +16,28 @@ function bytesToSize(bytes) {
   return Math.round(bytes / Math.pow(1024, i), 2) + ' ' + sizes[i];
 };
 
-function isBilibili(url) {
-    return url.indexOf('bilibili') !== -1;
-}
-
-function isYoutube(url) {
-    return url.indexOf('youtu') !== -1;
-}
-
-function getFormats(info) {
-    var formats = [];
-    info.formats.forEach(function(item) {
-        if(item.format_note !== 'DASH audio' && item.filesize) {
-            item.filesize = item.filesize ? bytesToSize(item.filesize) : 'unknown';
-            item.http_headers = item.http_headers ? JSON.stringify(item.http_headers) : 'unknown';
-            formats.push(item);
-        }
-    });
-    return formats;
-}
-
 var ytdl = require('youtube-dl');
-async function bilibiliHandler(req, res, next) {
+var pattern = /^((?:https?:)?\/\/)?((?:www|m)\.)?((?:youtube\.com|youtu.be))(\/(?:[\w\-]+\?v=|embed\/|v\/)?)([\w\-]+)(\S+)?$/;
+
+router.post('/video', async function(req, res, next) {
     var url = req.body.url,
-    formats = null; 
-
-    ytdl.getInfo(url, ['--youtube-skip-dash-manifest'], function(err, info) {
-        if(err) return res.render('listbilibilivideo', {error: 'The link you provided either not a valid url or it is not acceptable'});
-        formats = getFormats(info);
-
-        res.render('listbilibilivideo', {meta: {id: info.id, formats: formats}});
-    }) 
-}
-
-async function youtubeHandler(req, res, next) {
-    var pattern = /^((?:https?:)?\/\/)?((?:www|m)\.)?((?:youtube\.com|youtu.be))(\/(?:[\w\-]+\?v=|embed\/|v\/)?)([\w\-]+)(\S+)?$/;
-    var url = req.body.url,
-    formats = null;  
+    formats = [];
 
     request.get(url, function (err, resp, body) {
         // check if it is valid url
         if(pattern.test(resp.request.uri.href)) {
             ytdl.getInfo(url, ['--youtube-skip-dash-manifest'], function(err, info) {
                 if(err) return res.render('listvideo', {error: 'The link you provided either not a valid url or it is not acceptable'});
-                formats = getFormats(info);
         
+                // push all video formats for download (skipping audio)
+                info.formats.forEach(function(item) {
+                    if(item.format_note !== 'DASH audio' && item.filesize) {
+                        item.filesize = item.filesize ? bytesToSize(item.filesize): 'unknown';
+                        formats.push(item);
+                    }
+                });
                 res.render('listvideo', {meta: {id: info.id, formats: formats}});
-            }) 
+            })
         }
      
         else {
@@ -70,28 +45,5 @@ async function youtubeHandler(req, res, next) {
         }
      
     })
-}
-
-async function otherHandler(req, res, next) {
-    var url = req.body.url,
-    formats = null; 
-
-    ytdl.getInfo(url, ['--youtube-skip-dash-manifest'], function(err, info) {
-        if(err) return res.render('listvideo', {error: 'The link you provided either not a valid url or it is not acceptable'});
-        formats = getFormats(info);
-
-        res.render('listvideo', {meta: {id: info.id, formats: formats}});
-    }) 
-}
-
-router.post('/video', async function(req, res, next) {
-    var url = req.body.url;
-    if (isBilibili(url)) {
-        await bilibiliHandler(req, res, next);
-    } else if (isYoutube(url)) {
-        await youtubeHandler(req, res, next);
-    } else {
-        await otherHandler(req, res, next);
-    }
 })
 module.exports = router;
